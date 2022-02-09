1b7173a9932198101f38a285d81eb719b6ccdf21
KoendertNL <49477924+KoendertNL@users.noreply.github.com>
2022-02-09 19:33:24 +0100

    All button support
    Note to change the pin at the start, as mine is broken..

--- Arduino Game Controller Script/game_controller/game_controller.ino
+++ Arduino Game Controller Script/game_controller/game_controller.ino
@@ -2,27 +2,38 @@
 
 const int spacePin = 8;//spacebar
 
+const int colorPinOne = 3;//Color =
+const int colorPinTwo = 7;//Color = NOTE: CHANGE THIS PIN NUMBER TO 4 ON WORKING CONTROLLER!
+const int colorPinThree = 5;//Color =
+const int colorPinFour = 6;//Color =
+boolean colorPinOnePressed, colorPinTwoPressed, colorPinThreePressed, colorPinFourPressed;
+int colorButtonOne = 0;
+int colorButtonTwo = 0;
+int colorButtonThree= 0;
+int colorButtonFour = 0; 
+
 const int analogY = A0;//Analog stick Y   
 const int analogX = A1;//Analog stick X                   
 
 int spaceButton = 0;
-boolean buttonIsPressed, analogXPressed, analogYPressed;
+boolean buttonIsPressed;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(spacePin, INPUT_PULLUP );//spacebar
+  pinMode(spacePin, INPUT_PULLUP );
+  pinMode(colorPinOne, INPUT_PULLUP );
+  pinMode(colorPinTwo, INPUT_PULLUP );
+  pinMode(colorPinThree, INPUT_PULLUP );
+  pinMode(colorPinFour, INPUT_PULLUP );
 
 
   Serial.begin(9600);
-  // initialize mouse control:
+
   Keyboard.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  spaceButton = !digitalRead(spacePin);
-  Serial.println(analogRead(analogX)); 
-  
 
   //analogY
   if(analogRead(analogY)< 100){
@@ -54,15 +65,59 @@ void loop() {
   }
 
 
+  spaceButton = !digitalRead(spacePin);
   
   //button 1
   if (spaceButton == HIGH && !buttonIsPressed) {
      buttonIsPressed = true;
-     Serial.println("pressed"); 
+     
   }
   if(spaceButton == LOW && buttonIsPressed){
     buttonIsPressed = false;
-    Serial.println("not pressed"); 
     Keyboard.write(' ');
   }
+
+//All colors
+  colorButtonOne = !digitalRead(colorPinOne);
+  colorButtonTwo =  !digitalRead(colorPinTwo);
+  colorButtonThree =  !digitalRead(colorPinThree);
+  colorButtonFour = !digitalRead(colorPinFour);
+  //color 1
+  if (colorButtonOne == HIGH) {
+     colorPinOnePressed = true;
+  }
+  if(colorButtonOne == LOW && colorPinOnePressed){
+      colorPinOnePressed = false;
+    Keyboard.write('1');
+  }
+
+  //color 2
+  if (colorButtonTwo == HIGH) {
+     colorPinTwoPressed = true;
+  }
+  if(colorButtonTwo == LOW && colorPinTwoPressed){
+      colorPinTwoPressed = false;
+    Keyboard.write('2');
+  }
+
+  //color 3
+  if (colorButtonThree == HIGH) {
+     colorPinThreePressed = true;
+  }
+  if(colorButtonThree == LOW && colorPinThreePressed){
+      colorPinThreePressed = false;
+    Keyboard.write('3');
+  }
+
+  // color 4
+  if (colorButtonFour == HIGH) {
+     colorPinFourPressed = true;
+  }
+  if(colorButtonFour == LOW && colorPinFourPressed){
+      colorPinFourPressed = false;
+    Keyboard.write('4');
+  }
+  
+
+  
 }
---
+++ GXPEngine/ArduinoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+//using System.IO.Ports;
+using System.Threading;
+
+namespace GXPEngine
+{
+    public class ArduinoController
+    {
+       /* SerialPort port = new SerialPort();
+        port.PortName = "COM4"; 
+        port.BaudRate = 9600;
+        port.RtsEnable = true;
+        port.DtrEnable = true;
+        port.Open();
+        while (true)
+        {
+            string a = port.ReadExisting();
+            if (a!="") 
+                Console.WriteLine("Read from port: "+a);
+
+            if (Console.KeyAvailable) {
+                ConsoleKeyInfo key = Console.ReadKey();
+        port.Write(key.KeyChar.ToString());
+            }
+        Thread.Sleep(30);
+        }*/
+    }
+}
