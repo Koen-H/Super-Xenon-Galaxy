886c382d7b2a925bd55f0c645ff1b2141a4df611
Alan Safonov <alan.safonov@gmail.com>
2022-02-10 15:02:01 +0200

    HUD + PlayerData

---
+++ GXPEngine/PlayerData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GXPEngine
+{
+    /// <summary>
+    /// Player data class needed to use the data across all the code without messing it up.
+    /// </summary>
+    public class PlayerData
+    {
+        private int score;
+        private int hearts;
+
+        public PlayerData()
+        {
+            hearts = 3;
+            score = 0;
+        }
+
+        public int GetScore()
+        {
+            return score;
+        }
+
+        public int GetHearts()
+        {
+            return hearts;
+        }
+    }
+}
